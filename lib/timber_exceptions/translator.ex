6367e9abf85ab6269991c53390b2ced2236031ad
Mitchell Henke <mitchh23@gmail.com>
2019-01-30 12:51:54 -0600

    remove usage of ErrorEvent

--- lib/timber_exceptions/translator.ex
+++ lib/timber_exceptions/translator.ex
@@ -1,6 +1,4 @@
 defmodule Timber.Exceptions.Translator do
-  alias Timber.Events.ErrorEvent
-
   @moduledoc """
   This module implements a Logger translator to take advantage of
   the richer metadata available from Logger in OTP 21 and Elixir 1.7+.
@@ -21,6 +19,8 @@ defmodule Timber.Exceptions.Translator do
   Supervisor.start_link(children, opts)
   ```
   """
+
+  @max_backtrace_size 20
 
   def translate(min_level, level, kind, message) do
     case Logger.Translator.translate(min_level, level, kind, message) do
@@ -44,9 +44,9 @@ defmodule Timber.Exceptions.Translator do
 
   def transform_metadata(metadata) do
     with {:ok, crash_reason} <- Keyword.fetch(metadata, :crash_reason),
-         {:ok, event} <- get_error_event(crash_reason) do
+         {:ok, error} <- get_error(crash_reason) do
       event = %{
-        error: Map.from_struct(event)
+        error: error
       }
 
       Keyword.merge([event: event], metadata)
@@ -56,35 +56,75 @@ defmodule Timber.Exceptions.Translator do
     end
   end
 
-  defp get_error_event({{%{__exception__: true} = error, stacktrace}, _stack})
+  defp get_error({{%{__exception__: true} = error, stacktrace}, _stack})
        when is_list(stacktrace) do
-    {:ok, build_error_event(error, stacktrace, :error)}
+    {:ok, build_error(error, stacktrace)}
   end
 
-  defp get_error_event({%{__exception__: true} = error, stacktrace}) when is_list(stacktrace) do
-    {:ok, build_error_event(error, stacktrace, :error)}
+  defp get_error({%{__exception__: true} = error, stacktrace}) when is_list(stacktrace) do
+    {:ok, build_error(error, stacktrace)}
   end
 
-  defp get_error_event({{type, reason}, stacktrace}) when is_list(stacktrace) do
-    {:ok, build_error_event(reason, stacktrace, type)}
+  defp get_error({{_type, reason}, stacktrace}) when is_list(stacktrace) do
+    {:ok, build_error(reason, stacktrace)}
   end
 
-  defp get_error_event({error, stacktrace}) when is_list(stacktrace) do
-    {:ok, build_error_event(error, stacktrace, :error)}
+  defp get_error({error, stacktrace}) when is_list(stacktrace) do
+    {:ok, build_error(error, stacktrace)}
   end
 
-  defp get_error_event(_) do
+  defp get_error(_) do
     {:error, :no_info}
   end
 
-  defp build_error_event(%{__exception__: true} = error, stacktrace, _type) do
-    ErrorEvent.from_exception(error)
-    |> ErrorEvent.add_backtrace(stacktrace)
+  defp build_error(%{__exception__: true, __struct__: module} = error, stacktrace) do
+    message = Exception.message(error)
+    module_name = Timber.Utils.Module.name(module)
+
+    %{
+      message: message,
+      name: module_name,
+      backtrace: build_backtrace(stacktrace)
+    }
   end
 
-  defp build_error_event(error, stacktrace, _type) do
+  defp build_error(error, stacktrace) do
     ErlangError.normalize(error, stacktrace)
-    |> ErrorEvent.from_exception()
-    |> ErrorEvent.add_backtrace(stacktrace)
+    |> build_error(stacktrace)
+  end
+
+  defp build_backtrace([trace | _] = backtrace) when is_map(trace) do
+    Enum.slice(backtrace, 0..(@max_backtrace_size - 1))
+  end
+
+  defp build_backtrace([stack | _rest] = stacktrace) when is_tuple(stack) do
+    stacktrace_to_backtrace(stacktrace)
+  end
+
+  defp build_backtrace(_) do
+    []
+  end
+
+  defp stacktrace_to_backtrace(stacktrace) do
+    # arity is an integer or list of arguments
+    Enum.map(stacktrace, fn {module, function, arity, location} ->
+      arity =
+        case arity do
+          arity when is_list(arity) -> length(arity)
+          _ -> arity
+        end
+
+      file =
+        Keyword.get(location, :file)
+        |> Kernel.to_string()
+
+      line = Keyword.get(location, :line)
+
+      %{
+        function: Exception.format_mfa(module, function, arity),
+        file: file,
+        line: line
+      }
+    end)
   end
 end
